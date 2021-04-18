9c68586553ac61ede4bf8f4bb338c00b8e784d79
Luis Masuelli <luisfmasuelli@gmail.com>
2021-04-17 22:43:18 -0500

    Reordering package layout and also moving singleton behaviour

---
+++ Runtime/Authoring/Behaviours/SingletonBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AlephVault.Unity.Support.Generic
+{
+    namespace Authoring
+    {
+        namespace Behaviours
+        {
+            /// <summary>
+            ///   <para>
+            ///     A singleton class to serve as base type of singleton behaviours.
+            ///       Only one instance can be created for each singleton type and
+            ///       this is checked / enforced in the Awake() method, and released
+            ///       in the OnDestroy() method.
+            ///   </para>
+            ///   <para>
+            ///     Your children classes must be autorreferential (e.g. <c>public
+            ///       class Foo : SingletonBehaviour&lt;Foo&gt;</c>).
+            ///   </para>
+            /// </summary>
+            /// <typeparam name="T">The autorreferential type to ensure uniqueness</typeparam>
+            public abstract class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+            {
+                /// <summary>
+                ///   An exception class for singletons.
+                /// </summary>
+                public class SingletonException : Support.Types.Exception
+                {
+                    public SingletonException() { }
+                    public SingletonException(string message) : base(message) { }
+                    public SingletonException(string message, System.Exception inner) : base(message, inner) { }
+                }
+
+                /// <summary>
+                ///   The only instance allowed of this singleton.
+                /// </summary>
+                public static T Instance;
+
+                protected virtual void Awake()
+                {
+                    if (Instance == null)
+                    {
+                        Instance = this as T;
+                    }
+                    else
+                    {
+                        Destroy(gameObject);
+                        throw new SingletonException(string.Format("An instance of a Singleton<{0}>-derived class ({1}) is already created", typeof(T).FullName, GetType().FullName));
+                    }
+                }
+
+                protected virtual void OnDestroy()
+                {
+                    if (Instance == this) Instance = null;
+                }
+            }
+        }
+    }
+}
